1d89ac1eab220919eef31871b8cd4590891d0618
mugambiDaktari <wycliffmutethia@gmail.com>
2024-12-20 09:25:39 +0300

    First modification

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,5 +2,6 @@ square_length = int(input("Enter the size of the pattern:"))
 
 i = 1
 while i <= square_length:
-    print(square_length * "*")
+    print(square_length * "*", end="")
     i += 1
+    print()
