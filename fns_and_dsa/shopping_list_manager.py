53f2d6d51abc4c421eb197b0f372e297e8ad8726
mugambiDaktari <wycliffmutethia@gmail.com>
2025-01-10 12:26:36 +0300

    datastructures shopping list

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,41 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            item = input("Enter your shopping item: ")
+            if item in shopping_list:
+                print(f"{item} is already in your shopping list")
+            else:
+                shopping_list.append(item)
+                print(f"{item} has been added in your shopping list")
+        elif choice == '2':
+            # Prompt for and remove an item
+            itemToRemove = input("Enter the item to remove from shopping list: ")
+            if itemToRemove in shopping_list:
+                shopping_list.remove(itemToRemove)
+                print(f"{itemToRemove} has been removed from your shopping list")
+            else:
+                print(f"{itemToRemove} is not in your shopping list")
+            
+        elif choice == '3':
+            # Display the shopping list
+            print(shopping_list)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
