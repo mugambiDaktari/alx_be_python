f0cd9603c833e5aad2e29c09520f4b13c1467e57
mugambiDaktari <wycliffmutethia@gmail.com>
2025-01-19 10:40:32 +0300

    corrected implementation of display_balance

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -18,7 +18,7 @@ class BankAccount:
             return True
         
     def display_balance(self):
-        print(f"Current balance is ${self.account_balance}.")
+        return(f"Current balance is ${self.account_balance}.")
         #return self.account_balance
     
 
