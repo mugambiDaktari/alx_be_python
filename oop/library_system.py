4311036d6d8d89b306040c32249fef4ba14f2c65
mugambiDaktari <wycliffmutethia@gmail.com>
2025-01-25 14:26:23 +0300

    added __str__ to every class as per requirements of ALX

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,6 +5,9 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"    
+
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
@@ -12,12 +15,18 @@ class EBook(Book):
             raise TypeError("File size must be an integer")
         self.file_size = file_size
 
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File size: {self.file_size}KB"
+
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         if not isinstance(page_count, int):
             raise TypeError("Page count must be an integer")
         self.page_count = page_count
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page count: {self.page_count}"
 
 class Library:
     books = []
@@ -31,13 +40,7 @@ class Library:
     @classmethod
     def list_books(cls):
         for book in cls.books:
-            book_type = type(book).__name__
-            if isinstance(book, EBook):
-                print(f"{book_type}: {book.title} by {book.author}, File size: {book.file_size}KB")
-            elif isinstance(book, PrintBook):
-                print(f"{book_type}: {book.title} by {book.author}, Page count: {book.page_count}")
-            else:
-                print(f"{book_type}: {book.title} by {book.author}")  
+            print(str(book)) 
 
 
 
