fbc0f7f04c3cb2bf54c093a885148b0f15490369
mugambiDaktari <wycliffmutethia@gmail.com>
2025-01-25 17:06:00 +0300

    Capitalized two letters in a string

--- oop/library_system.py
+++ oop/library_system.py
@@ -16,7 +16,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"EBook: {self.title} by {self.author}, File size: {self.file_size}KB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
@@ -26,7 +26,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"PrintBook: {self.title} by {self.author}, Page count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 
@@ -40,5 +40,5 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(book)  
+            print(book)
  
